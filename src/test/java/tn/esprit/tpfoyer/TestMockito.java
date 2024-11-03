339b7a30d695b774fee3c0b1c55bdf3429052a83
YassinBouziri <bouziriyassin3@gmail.com>
2024-11-03 14:17:12 +0100

    correction test

--- src/test/java/tn/esprit/tpfoyer/TestMockito.java
+++ src/test/java/tn/esprit/tpfoyer/TestMockito.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-public class TestMockito {
+ class TestMockito {
 
     @Mock
     private FoyerRepository foyerRepository;
